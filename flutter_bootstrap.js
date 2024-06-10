2d4dc2afcf46c7a35d673ced44b9e999bab8bf56
Abi <4ndreiene@gmail.com>
2024-06-10 11:44:17 +0300

    see popup with screenshots on desktop

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1287839417"
+    serviceWorkerVersion: "3600056216"
   }
 });
